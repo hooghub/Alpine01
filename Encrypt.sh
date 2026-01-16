459f867ca5d967f69ff920c3e69a18705ecaca62
hooghub <goookuooog@gmail.com>
2026-01-16 04:14:48 -0800

    Update Encrypt.sh

--- Encrypt.sh
+++ Encrypt.sh
@@ -562,7 +562,7 @@ cat > "$CONFIG_PATH" <<EOF
     {
       "type": "vless",
       "tag": "vless-reality",
-      "listen": "0.0.0.0",
+      "listen": "::",
       "listen_port": ${VLESS_REALITY_PORT},
       "users": [
         { "uuid": "${UUID}",
@@ -586,7 +586,7 @@ cat > "$CONFIG_PATH" <<EOF
     {
       "type": "vless",
       "tag": "in-vless-tls",
-      "listen": "0.0.0.0",
+      "listen": "::",
       "listen_port": ${VLESS_TLS_PORT},
       "users": [
         { "uuid": "${UUID}" }
@@ -601,7 +601,7 @@ cat > "$CONFIG_PATH" <<EOF
     {
       "type": "hysteria2",
       "tag": "in-hy2",
-      "listen": "0.0.0.0",
+      "listen": "::",
       "listen_port": ${HY2_PORT},
       "users": [
         { "password": "${HY2_PASSWORD}" }
